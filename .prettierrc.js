5537e73465296d07296a79b061a21f3f9bce82c3
Lorenzo Sijinardi <lorenzo.sijinardi@accenture.com>
2025-05-08 17:59:51 +0200

    fix custom settings in .prettierrc + eslint.config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// .prettierrc.js
+module.exports = {
+  // Disabilito i punti e virgola
+  semi: false,
+
+  // Preferisco le virgolette singole
+  singleQuote: true,
+
+  // Virgola finale dove supportata da ES5
+  trailingComma: "es5",
+};
--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import importPlugin from 'eslint-plugin-import'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 
 export default [
   { ignores: ['dist'] },
@@ -19,6 +20,7 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: importPlugin,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -27,6 +29,28 @@ export default [
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+      ],
+      'object-curly-spacing': ['error', 'always'],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // es. fs, path
+            'external', // es. react, lodash
+            'internal', // es. @/components
+            ['parent', 'sibling', 'index'],
+          ],
+          pathGroups: [
+            {
+              pattern: '*.css',
+              group: 'index',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
       ],
     },
   },
